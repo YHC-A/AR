0b992478acb5f2a43af0a5bae4e0d9caea9b671a
Y.H Chen <999benson@gmail.com>
2020-07-26 01:23:47 +0800

    omega_finish

--- ANGLE/ANGLE.ino
+++ ANGLE/ANGLE.ino
@@ -1,13 +1,14 @@
 double distance1 = 100;
 double distance2 = 96;
-double distance3;
-double trigBetween = 10;
-double wheelBetween = 60;
-int T_Catch_up = 2;
-
 //  以上單位為 cm, sec
 
 int angle_date(float x, float y){
+
+  double trigBetween = 10;
+  double wheelBetween = 60;
+  int T_Catch_up = 2;
+  double distance3;
+  
   Serial.begin(9600);
   
   double ANG1 = acos((pow(distance1, 2) + pow(trigBetween, 2) - pow(distance2, 2)) / (2 * distance1 * trigBetween)) * 180 / PI ;
@@ -27,8 +28,17 @@ int angle_date(float x, float y){
   Serial.println("角度2(R) = " + String(ANG2));
   Serial.println("角度3(target, 單位 : 度) = " + String(ANG3));
   Serial.println("distance3(target, 單位 : cm) = " + String(distance3));
-  
-  
+
+  float omega = ANG3 * PI / 180 / T_Catch_up;
+
+  Serial.println("轉速 = " + String(omega) + "rad/s");
+
+  float V1_plus = omega * wheelBetween / 2;
+  float V2_plus = omega * wheelBetween / 2 * (-1);
+
+  Serial.println(V1_plus);
+  Serial.println(V2_plus);
+
 }
 
 
